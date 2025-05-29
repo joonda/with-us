7d733861f2ab6b26e14cadaa1505e0c08fa4caac
joonda <kloudzero@kakao.com>
2025-05-29 23:25:52 +0900

    :lipstick: [Design] add main banner processing...

--- src/layouts/View.vue
+++ src/layouts/View.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class='flex justify-center'>
     <RouterView />
   </div>
 </template>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,28 @@
 <template>
-  <p class="text-4xl">메인 입니다</p>
+  <div class="p-4">
+    <div class="relative w-[970px] h-[360px]">
+      <img
+          src="../assets/bg.jpg"
+          alt="background"
+          class="w-full h-full object-cover shadow-inner "
+      >
+      <div
+        class="absolute top-18 left-20 text-white"
+      >
+        <p class="text-4xl">누군가와 함께할 때, <br /> 더 특별해져요</p>
+        <p class="mt-4">처음 가는 행사도, 덜 어색할 수 있어요. <br />
+          혼자 고민하지 말고, 함께 갈 사람을 찾아보세요. <br />
+          같이행이 그 시작을 도와줄게요.</p>
+        <button
+            type="button"
+            class="mt-2 bg-blue-500 px-4 py-2 rounded-md cursor-pointer hover:bg-blue-600 transition"
+        >
+          바로 행사 찾기
+        </button>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
