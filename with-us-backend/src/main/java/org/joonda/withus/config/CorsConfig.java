d5c4f8cda1aa8e91621855828174c957be997a0d
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-06-03 14:41:36 +0900

    ♻️ [Refactor] change directory structure
    [Refactor] change directory structure

---
+++ with-us-backend/src/main/java/org/joonda/withus/config/CorsConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Cors filter setting
+ * */
+
+package org.joonda.withus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // vue address
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // allowed methods
+        config.setAllowedHeaders(List.of("*")); // allowed headers
+        config.setAllowCredentials(true); // allowed cookie, authentication
+
+        // 모든 경로에 대해 위에서 설정한 CORS 적용.
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
