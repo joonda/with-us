32e4b4c0d4cb45b106650719f9f293b3d59d6b64
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-06-07 19:17:56 +0900

    :sparkles: [Feat] 회원가입, axios 호출 및 git ignore 추가

--- with-us-frontend/src/views/SignUpView.vue
+++ with-us-frontend/src/views/SignUpView.vue
@@ -20,7 +20,8 @@
       </div>
       <div>
         <label for="checkPassword" class="block mb-1">비밀번호 확인</label>
-        <input id="checkPassword" type="password" class="p-4 w-full border border-gray-300 rounded-md"
+        <input v-model="confirmPassword" id="checkPassword" type="password"
+               class="p-4 w-full border border-gray-300 rounded-md"
                placeholder="확인을 위해 한번 더 입력해주세요."/>
       </div>
       <div>
@@ -31,14 +32,16 @@
         <label for="birthdate" class="block mb-1">생년월일</label>
         <div id="birthdate" class="flex gap-2">
           <select v-model="form.birthYear" class="px-4 py-2 w-full border rounded-md border-gray-300">
-            <option value="">선택</option>
-            <option>1995</option>
+            <option value="">연도</option>
+            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
           </select>
           <select v-model="form.birthMonth" class="px-4 py-2 w-full border rounded-md border-gray-300">
-            <option value="">선택</option>
+            <option value="">월</option>
+            <option v-for="month in 12" :key="month" :value="month.toString().padStart(2, '0')">{{ month }}</option>
           </select>
           <select v-model="form.birthDay" class="px-4 py-2 w-full border rounded-md border-gray-300">
-            <option value="">선택</option>
+            <option value="">일</option>
+            <option v-for="day in daysInMonth" :key="day" :value="day.toString().padStart(2, '0')">{{ day }}</option>
           </select>
         </div>
       </div>
@@ -55,6 +58,7 @@
       </div>
       <button
           type="button"
+          @click="submitForm"
           class="mt-2 bg-[#569AFF] py-4 rounded-lg cursor-pointer hover:bg-[#4383e0] transition text-white"
       >
         확인
@@ -64,7 +68,11 @@
 </template>
 
 <script setup lang="ts">
-import {reactive} from "vue";
+import {computed, reactive, ref} from "vue";
+import {useRouter} from "vue-router";
+import axiosInstance from "../lib/axios.ts";
+
+const router = useRouter();
 
 interface SignUpForm {
   identify: string
@@ -87,6 +95,67 @@ const form = reactive<SignUpForm>({
   phone: '',
   gender: ''
 })
+
+const confirmPassword = ref('')
+
+const years = computed(() => {
+  const currentYear = new Date().getFullYear()
+  const yearList = []
+  for (let i = currentYear; i >= currentYear - 100; i--) {
+    yearList.push(i.toString())
+  }
+
+  return yearList
+})
+
+const daysInMonth = computed(() => {
+  if (!form.birthYear || !form.birthMonth) return 31
+  const daysCount = new Date(parseInt(form.birthYear), parseInt(form.birthMonth), 0).getDate()
+  return Array.from({length: daysCount}, (_, i) => i + 1)
+})
+
+const birthDate = computed(() => {
+  if (form.birthYear && form.birthMonth && form.birthDay) {
+    return `${form.birthYear}-${form.birthMonth}-${form.birthDay}`
+  }
+
+  return null
+})
+
+const submitForm = async () => {
+  try {
+    if (form.identify.trim() === '') {
+      alert('아이디(이메일)을 입력해주세요')
+      return
+    }
+
+    if (!birthDate.value) {
+      alert('생년월일을 입력해주세요')
+      return
+    }
+
+    if (form.gender === '') {
+      alert('성별을 선택해주세요')
+      return
+    }
+
+    const data = {
+      email: form.identify,
+      password: form.password,
+      name: form.name,
+      birthDate: birthDate.value,
+      phone: form.phone,
+      gender: form.gender,
+    }
+
+    console.log("전송 데이터 → ", data)
+    const response = await axiosInstance.post('/users/signup', data)
+    alert(response.data.message)
+    await router.push('/login')
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <style scoped>
